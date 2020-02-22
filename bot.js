310b7fa1aad0f19189980352e468506b05e2f64f
cyla00 <cyla00@protonmail.com>
2020-02-22 23:59:28 +0100

    quotes

--- bot.js
+++ bot.js
@@ -1,18 +1,20 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '996898831:AAEU_2mk3oBmWx_ZI8axooyZXKeweyT5cng';
+const token = '996898831:AAHerDdP3Rmlqw3GuRzd34hiQ7r6VEmXjMQ';
 const bot = new TelegramBot(token, {polling: true});
 
-
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const resp = match[1];
-  bot.sendMessage(chatId, resp);
-});
-
+              // comando + risposta START
 bot.onText(/\/start/, (msg) => {
-bot.sendMessage(msg.chat.id, "Eccomi, come ti posso aiutare..?");
+bot.sendMessage(msg.chat.id, "\"Sono tanto semplici li uomini, e tanto obediscono alle necessità presenti, che colui che inganna troverà sempre chi si lascerà ingannare☕...\"");
 });
 
+            // comandi basici bot
+
+// ciao
 bot.onText(/\/ciao/, (msg) => {
 bot.sendMessage(msg.chat.id, "hey!");
+});
+
+// machiavelli
+bot.onText(/\/machiavelli/, (msg) => {
+bot.sendMessage(msg.chat.id, "\"Consigliati delle cose che tu dei fare con molti; quello che dipoi vuoi fare conferisci con pochi☕...\"");
 });
