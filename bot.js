a14795f6e18db30cb28672380913b3f1f5c81c0e
cyla00 <cyla00@protonmail.com>
2020-03-12 11:54:53 +0100

    inizio

--- bot.js
+++ bot.js
@@ -1,9 +1,25 @@
 'use strict'
 
 const Telegram = require('telegram-node-bot');
+const TelegramBot = require('node-telegram-bot-api');
+const settingsJson = require("./settings.json");
 const TelegramBaseController = Telegram.TelegramBaseController;
 const TextCommand = Telegram.TextCommand;
-const settingsJson = require("./settings.json");
-const settingsBat = require("./settings.bat");
 const token = settingsJson.token;
-const tg = new Telegram.Telegram(token);
+const  bot = new Telegram.Telegram(token);
+const ero = new TelegramBot(token, {polling: true});
+
+
+ero.on('message', (msg) => {
+
+const hi = "ciao";
+if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
+ero.sendMessage(msg.chat.id,"Hello dear user");
+}
+
+const bye = "bye";
+if (msg.text.toString().toLowerCase().includes(bye)) {
+ero.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
+}
+
+});
