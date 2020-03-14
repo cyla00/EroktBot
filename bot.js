917bfca52a08e6fee55cf755163bbd8b1bf20232
cyla00 <cyla00@protonmail.com>
2020-03-14 18:52:02 +0100

    ogs firebase

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 
-
 const TelegramBot = require('node-telegram-bot-api');
+const ogs = require('open-graph-scraper');
+const firebase = require('firebase');
 const settings = require("./settings.json");
 const token = settings.token;
 const bot  = new TelegramBot(token, {polling:true});
